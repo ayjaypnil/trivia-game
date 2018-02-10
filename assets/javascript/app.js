c2d726168e43d2674df7a82bc6910f24089f3550
Anil Patel <AnilJayPatel@Anils-MacBook-Pro-2.local>
2018-02-10 15:19:45 -0600

    timers and questions set, need to work on logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,7 @@
-
-
 var object = {
     questionOne: {
-        question: "What number is Javier Baez?", 
-        answerChoices: ["9", "23", "16", "21"], 
+        question: "What number is Javier Baez?",
+        answerChoices: ["9", "23", "16", "21"],
         answer: "9",
     },
     questionTwo: {
@@ -28,103 +26,148 @@ var object = {
     }
 };
 
-
-
+var intervalID;
 var showQuestion;
-var count = 0;
-
-$("#startButton").on("click", startQuestion);
+var clockRunning = false;
 
 
-function startQuestion() {
 
-    $("#question").append(object.questionOne.question)
-    for (i = 0; i < object.questionOne.answerChoices.length; i++) {
-        answerBtn = $("<button>");
-        answerBtn.addClass("letter-button letter letter-button-color");
-        answerBtn.attr("data-letter", object.questionOne.answerChoices[i]);
-        answerBtn.text(object.questionOne.answerChoices[i]);
-        $("#answer").append(answerBtn);
-    }
-    setTimeout(nextQuestionTwo, 2000);
-   
-};
+var stopwatch = {
 
+    time: 5,
 
-function nextQuestionTwo(){
+    start: function () {
+        if (!clockRunning) {
+            intervalId = setInterval(stopwatch.count, 1000);
+            clockRunning = true;
+        }
+    },
+    count: function () {
+        stopwatch.time--;
+        $("#timer").text(stopwatch.time);
+    },
 
-    $("#question").empty();
-    $("#answer").empty();
-    $("#question").append(object.questionTwo.question)
-    for (i = 0; i < object.questionTwo.answerChoices.length; i++) {
-        answerBtn = $("<button>");
-        answerBtn.addClass("letter-button letter letter-button-color");
-        answerBtn.attr("data-letter", object.questionTwo.answerChoices[i]);
-        answerBtn.text(object.questionTwo.answerChoices[i]);
-        $("#answer").append(answerBtn);
-    }
-    setTimeout(nextQuestionThree, 2000);
-};
+    startQuestion: function() {
+        $("#question").append(object.questionOne.question)
 
+        for (i = 0; i < object.questionOne.answerChoices.length; i++) {
+            answerBtn = $("<button>");
+            answerBtn.addClass("btn btn-default");
+            answerBtn.attr("data-letter", object.questionOne.answerChoices[i]);
+            answerBtn.text(object.questionOne.answerChoices[i]);
 
-function nextQuestionThree() {
+            $("#answer").append(answerBtn);
+            $("#timer").text(parseInt(5));
 
-    $("#question").empty();
-    $("#answer").empty();
-    $("#question").append(object.questionThree.question)
-    for (i = 0; i < object.questionThree.answerChoices.length; i++) {
-        answerBtn = $("<button>");
-        answerBtn.addClass("letter-button letter letter-button-color");
-        answerBtn.attr("data-letter", object.questionThree.answerChoices[i]);
-        answerBtn.text(object.questionThree.answerChoices[i]);
-        $("#answer").append(answerBtn);
-    }
-    setTimeout(nextQuestionFour, 2000);
-};
+            // $(".btn").on("click", function(){
+            //     console.log("clicked");
+            // });
+        }
+        stopwatch.start();
+        setTimeout(stopwatch.nextQuestionTwo, 5 * 1000);
+    },
 
-function nextQuestionFour() {
+    nextQuestionTwo: function () {
 
-    $("#question").empty();
-    $("#answer").empty();
-    $("#question").append(object.questionFour.question)
-    for (i = 0; i < object.questionFour.answerChoices.length; i++) {
-        answerBtn = $("<button>");
-        answerBtn.addClass("letter-button letter letter-button-color");
-        answerBtn.attr("data-letter", object.questionFour.answerChoices[i]);
-        answerBtn.text(object.questionFour.answerChoices[i]);
-        $("#answer").append(answerBtn);
-    }
-    setTimeout(nextQuestionFive, 2000);
-};
+        stopwatch.time = 5;
+        $("#timer").text(parseInt(5));
 
-function nextQuestionFive() {   
+        $("#question").empty();
+        $("#answer").empty();
+        $("#question").append(object.questionTwo.question)
 
-    $("#question").empty();
-    $("#answer").empty();
-    $("#question").append(object.questionFive.question)
-    for (i = 0; i < object.questionFive.answerChoices.length; i++) {
-        answerBtn = $("<button>");
-        answerBtn.addClass("letter-button letter letter-button-color");
-        answerBtn.attr("data-letter", object.questionFive.answerChoices[i]);
-        answerBtn.text(object.questionFive.answerChoices[i]);
-        $("#answer").append(answerBtn);
-    }
-    setTimeout(triviaOver, 2000);
-};
-
-function triviaOver(){
+        for (i = 0; i < object.questionTwo.answerChoices.length; i++) {
+            answerBtn = $("<button>");
+            answerBtn.addClass("btn btn-default");
+            answerBtn.attr("data-letter", object.questionTwo.answerChoices[i]);
+            answerBtn.text(object.questionTwo.answerChoices[i]);
+            $("#answer").append(answerBtn);
+        }
     
-    $("#question").empty();
-    $("#answer").empty();
-    $("h1").empty();
+        stopwatch.start();
+        setTimeout(stopwatch.nextQuestionThree, 5 * 1000);
+    },
+    
+    nextQuestionThree: function () {
 
-    $("h1").append("Trivia is Over");
-    $("#question").append("You got this many right");
-    $("#answer").append("You got this many wrong");
+        stopwatch.time = 5;
+        $("#timer").text(parseInt(5));
+
+        $("#question").empty();
+        $("#answer").empty();
+        $("#question").append(object.questionThree.question)
+        for (i = 0; i < object.questionThree.answerChoices.length; i++) {
+            answerBtn = $("<button>");
+            answerBtn.addClass("btn btn-default");
+            answerBtn.attr("data-letter", object.questionThree.answerChoices[i]);
+            answerBtn.text(object.questionThree.answerChoices[i]);
+            $("#answer").append(answerBtn);
+        }
+        stopwatch.start();
+        setTimeout(stopwatch.nextQuestionFour, 5 * 1000);
+    },
+
+    nextQuestionFour: function () {
+
+        stopwatch.time = 5;
+        $("#timer").text(parseInt(5));
+
+        $("#question").empty();
+        $("#answer").empty();
+        $("#question").append(object.questionFour.question)
+        for (i = 0; i < object.questionFour.answerChoices.length; i++) {
+            answerBtn = $("<button>");
+            answerBtn.addClass("btn btn-default");
+            answerBtn.attr("data-letter", object.questionFour.answerChoices[i]);
+            answerBtn.text(object.questionFour.answerChoices[i]);
+            $("#answer").append(answerBtn);
+        }
+        stopwatch.start();
+        setTimeout(stopwatch.nextQuestionFive, 5 * 1000);
+    },
+
+    nextQuestionFive: function () {
+
+        stopwatch.time = 5;
+        $("#timer").text(parseInt(5));
+
+        $("#question").empty();
+        $("#answer").empty();
+        $("#question").append(object.questionFive.question)
+        for (i = 0; i < object.questionFive.answerChoices.length; i++) {
+            answerBtn = $("<button>");
+            answerBtn.addClass("btn btn-default");
+            answerBtn.attr("data-letter", object.questionFive.answerChoices[i]);
+            answerBtn.text(object.questionFive.answerChoices[i]);
+            $("#answer").append(answerBtn);
+        }
+        stopwatch.start();
+        setTimeout(stopwatch.triviaOver, 5 * 1000);
+    },
+
+
+    triviaOver: function () {
+
+        clearInterval(intervalId);
+        stopwatch.time;
+        clockRunning = false;
+        $("#timer").empty();
+
+        $("#question").empty();
+        $("#answer").empty();
+        $("h1").empty();
+
+        $("h1").append("Trivia is Over");
+        $("#question").append("You got this many right");
+        $("#answer").append("You got this many wrong");
+    },
+
 
 };
 
 
+
+$("#startButton").on("click", stopwatch.startQuestion);
 
 
 
@@ -161,9 +204,9 @@ function triviaOver(){
 //         nextQuestionBtn = $("<button> Next" + "</Button>");
 //         nextQuestionBtn.addClass("btn btn-default");
 //         $("#startButton").html(nextQuestionBtn);
-    
-        
-        
+
+
+
 // });
 
 
@@ -182,22 +225,32 @@ function triviaOver(){
 
 
 
-        // // Next question button goes, so that replaces the current start button
-        // nextQuestionBtn = $("<button> Next" + "</Button>");
-        // nextQuestionBtn.addClass("btn btn-default");
-        // $("#startButton").html(nextQuestionBtn);
+// // Next question button goes, so that replaces the current start button
+// nextQuestionBtn = $("<button> Next" + "</Button>");
+// nextQuestionBtn.addClass("btn btn-default");
+// $("#startButton").html(nextQuestionBtn);
 
-        // // click function on the next button so that next question pops up
-        // $(nextQuestionBtn).on("click", function () {
-        //     // clear the current question
-        //     $("#question").innerHTML();
-        //     // will need to save initial #question to a variable and reevaluate it**
-        //     $("#question").ht(object.questionTwo.question);
+// // click function on the next button so that next question pops up
+// $(nextQuestionBtn).on("click", function () {
+//     // clear the current question
+//     $("#question").innerHTML();
+//     // will need to save initial #question to a variable and reevaluate it**
+//     $("#question").ht(object.questionTwo.question);
 
-        // });
-
-
+// });
 
 
 
+// var clock = {
+//     time: 10,
+//     count: function () {
+//         clock.time--;
 
+//     }
+//         if(!clockRunning) {
+//         intervalId = setInterval(clock.count, 1000);
+//         clockRunning = true;
+//         $("#timer").text(clock.time);
+//     }
+
+// };
